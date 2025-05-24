620cf759f9f67ce791f134d6700ff2b9b7435bbe
arcathrax <arcathrax@proton.me>
2025-05-24 22:41:46 +0200

    fix: changed, so that only the necessary files get changed

--- setup.sh
+++ setup.sh
@@ -17,7 +17,10 @@ brew install FelixKratz/formulae/sketchybar
 echo "Cloning config..."
 git clone https://github.com/arcathrax/arcathrax-dotfiles.git /tmp/dotfiles
 mv $HOME/.config $HOME/.config_backup
-mv /tmp/dotfiles $HOME/.config
+mv /tmp/dotfiles/sketchybar $HOME/.config/sketchybar
+mv /tmp/dotfiles/skhd $HOME/.config/skhd
+mv /tmp/dotfiles/yabai $HOME/.config/yabai
+mv /tmp/dotfiles/yazi $HOME/.config/yazi
 rm -rf /tmp/dotfiles
 
 echo "Starting services"
